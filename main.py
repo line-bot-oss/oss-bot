ca25316646300615f9feb03e08c159ad97c15eb0
nanato12 <admin@nanato12.info>
2021-07-01 00:29:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from typing import Callable, List
-from akad.ttypes import OpType
+from typing import Coroutine
+from cherline.cherline.TalkService.ttypes import OpType, Operation
 from argparse import ArgumentParser
 
 from cherline import AppType as CherlineAppType
@@ -9,15 +9,16 @@ from settings import AppType, Environment, Setting
 import asyncio, traceback
 
 
-async def test() -> None:
-    print("ok")
+async def execute(client: Cherline, op: Operation) -> Coroutine:
+    if op.type == OpType.RECEIVE_MESSAGE:
+        return await client.sendMessage(op.message._from, op.message.text)
 
 
 def login(setting: Setting) -> LINE:
     """LINEにログインする関数
 
     Args:
-        setting (Setting): Setting
+        setting (Setting): 設定クラス
 
     Returns:
         LINE: LINEBot Client
@@ -52,6 +53,15 @@ def login(setting: Setting) -> LINE:
 
 
 def login_with_cherline(client: LINE, setting: Setting) -> Cherline:
+    """cherlineでログインする
+
+    Args:
+        client (LINE): LINEBot Client
+        setting (Setting): 設定クラス
+
+    Returns:
+        Cherline: Cherlnie Client
+    """
     cherline_client = Cherline(CherlineAppType[setting.app_type.name])
     cherline_client.login(client.authToken)
     return cherline_client
@@ -67,6 +77,7 @@ async def run(client: Cherline) -> None:
             revisions = []
             op_interrput: list = []
             for op in ops:
+                print(op)
                 if op.type == OpType.END_OF_OPERATION:
                     if op.param1:
                         a, _, _ = op.param1.partition("")
@@ -76,14 +87,16 @@ async def run(client: Cherline) -> None:
                         client.globalRev = int(a)
                 else:
                     revisions.append(op.revision)
-                if op.type == OpType.SEND_MESSAGE:
-                    op_interrput.append(test)
+
+                op_interrput.append(execute(client, op))
 
             if revisions:
                 client.setRevisions(revisions)
+
             if op_interrput:
                 task = asyncio.gather(*op_interrput)
                 await task
+
         except Exception as _:
             traceback.print_exc()
 
@@ -104,7 +117,6 @@ if __name__ == "__main__":
     setting = Setting(Environment(options.env), AppType.WINDOWS)
     client = login(setting)
     cherline_client = login_with_cherline(client, setting)
-    print(cherline_client.token)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(cherline_client))
