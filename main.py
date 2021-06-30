31773b3ec929c174faeaea314c7f64a56a528e5d
nanato12 <admin@nanato12.info>
2021-06-30 13:51:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from typing import Callable, List
+from akad.ttypes import OpType
 from argparse import ArgumentParser
 
 from cherline import AppType as CherlineAppType
 from cherline import Cherline
 from line import LINE
 from settings import AppType, Environment, Setting
+import asyncio, traceback
+
+
+async def test() -> None:
+    print("ok")
 
 
 def login(setting: Setting) -> LINE:
@@ -50,12 +57,47 @@ def login_with_cherline(client: LINE, setting: Setting) -> Cherline:
     return cherline_client
 
 
+async def run(client: Cherline) -> None:
+    while True:
+        try:
+            try:
+                ops = await client.fetchOps()
+            except AttributeError:
+                continue
+            revisions = []
+            op_interrput: list = []
+            for op in ops:
+                if op.type == OpType.END_OF_OPERATION:
+                    if op.param1:
+                        a, _, _ = op.param1.partition("")
+                        client.individualRev = int(a)
+                    if op.param2:
+                        a, _, _ = op.param2.partition("")
+                        client.globalRev = int(a)
+                else:
+                    revisions.append(op.revision)
+                if op.type == OpType.SEND_MESSAGE:
+                    op_interrput.append(test)
+
+            if revisions:
+                client.setRevisions(revisions)
+            if op_interrput:
+                task = asyncio.gather(*op_interrput)
+                await task
+        except Exception as _:
+            traceback.print_exc()
+
+
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage="python " + __file__ + " [--env <enviroment>] [--help]"
     )
     arg_parser.add_argument(
-        "-e", "--env", help="enviroment [dev, stg, prod]", nargs="?", required=True
+        "-e",
+        "--env",
+        help="enviroment [dev, stg, prod]",
+        nargs="?",
+        required=True,
     )
     options = arg_parser.parse_args()
 
@@ -63,3 +105,6 @@ if __name__ == "__main__":
     client = login(setting)
     cherline_client = login_with_cherline(client, setting)
     print(cherline_client.token)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(cherline_client))
